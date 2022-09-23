3613a91816d1d33964061bab193b349b884d105b
Julio Carpio <julio_carpio4@hotmail.com>
2022-09-22 18:11:46 -0600

    API v2 DI approach

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import { scopePerRequest } from 'awilix-express'
 
-import { router } from '../app/v1/routes/catRoutes.js'
+import routesHandler from '../app/v2/routes/catRoutes.js'
+import { createAppContainer } from '../app/v2/containers/appFactory.js'
+
 
 const PORT = process.env.PORT || 3001
 const HOST = '0.0.0.0'
@@ -11,9 +14,11 @@ const corsOptions = {
 }
 
 const app = express()
+const container = await createAppContainer()
 
 app.use(bodyParser.json())
 app.use(cors(corsOptions))
-app.use('/api/v1/cats', router)
+app.use(scopePerRequest(container))
+app.use('/api/v2/cats', routesHandler())
 
 app.listen(PORT, HOST);
